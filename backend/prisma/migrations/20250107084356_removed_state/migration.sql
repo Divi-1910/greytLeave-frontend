c230d66f26b7e70142efc40e0ba47b2fd84a1bbe
Divi-1910 <116071185+Divi-1910@users.noreply.github.com>
2025-02-14 22:21:28 +0530

    latest

--- backend/prisma/migrations/20250107084356_removed_state/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `state` on the `Country` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Country" DROP COLUMN "state";
--- backend/prisma/migrations/20250110042353_added_buhead/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Employee" ADD COLUMN     "BUHeadId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Employee" ADD CONSTRAINT "Employee_BUHeadId_fkey" FOREIGN KEY ("BUHeadId") REFERENCES "Employee"("id") ON DELETE SET NULL ON UPDATE CASCADE;
